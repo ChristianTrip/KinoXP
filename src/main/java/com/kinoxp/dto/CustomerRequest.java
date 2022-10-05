3f8f0928a541cdfd93d18ecc3e1a4bd61cbb5712
Robert Witt Hansen <along_the_black_river@hotmail.com>
2022-10-05 12:12:53 +0200

    customer classes and Reservation

---
+++ src/main/java/com/kinoxp/dto/CustomerRequest.java
@@ -0,0 +1,41 @@
+package com.kinoxp.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kinoxp.entities.Customer;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//---------------Lombok.
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+//---------------Lombok.
+
+public class CustomerRequest {
+    //---------------Fields / attributter.
+    private int id;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+    //---------------Fields / attributter.
+
+    //---------------Constructor.
+    //TODO how do this work
+    public static Customer getCustomerEntity(CustomerRequest cRequest){
+        return new Customer();
+    }
+    //TODO for Test in mock / H2
+    public CustomerRequest(Customer cRequest) {
+        this.id = cRequest.getId();
+        this.name = cRequest.getName();
+        this.phone = cRequest.getPhone();
+        this.email = cRequest.getEmail();
+    }
+    //---------------Constructor.
+}
