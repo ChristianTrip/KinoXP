aad26675582e85df41fb2b48bf1073d33042b76b
tLangelo <theislangelo@gmail.com>
2022-10-05 10:07:16 +0200

    added showservice and changes to showresponse

--- src/main/java/com/kinoxp/dto/ShowResponse.java
+++ src/main/java/com/kinoxp/dto/ShowResponse.java
@@ -1,4 +1,40 @@
 package com.kinoxp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kinoxp.entities.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ShowResponse {
+
+    private int id;
+    private Theaters theater;
+    private ShowingTimes showingTime;
+    private Movie movie;
+
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+    private LocalDateTime created;
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss",shape = JsonFormat.Shape.STRING)
+    private LocalDateTime edited;
+
+    public ShowResponse(Show show, boolean includeAll){
+        this.id = show.getId();
+        this.theater = show.getTheater();
+        this.showingTime = show.getShowingTimes();
+        this.movie = show.getMovie();
+        if(includeAll){
+            this.created = movie.getCreated();
+            this.edited = movie.getEdited();
+        }
+    }
 }
---
+++ src/main/java/com/kinoxp/services/ShowService.java
@@ -0,0 +1,28 @@
+package com.kinoxp.services;
+
+import com.kinoxp.dto.ShowResponse;
+import com.kinoxp.entities.Show;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ShowService {
+
+    private ShowRepository repository;
+
+    /*public ShowService(ShowRepository repository){
+        this.repository = repository;
+    }
+     */
+
+    public List<ShowResponse> getAllShows(){
+        List<Show> shows = repository.findAll();
+        List<ShowResponse> showResponses = shows
+                .stream()
+                .map(show -> new ShowResponse(show, true))
+                .collect(Collectors.toList());
+        return showResponses;
+    }
+}
