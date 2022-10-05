db6ed69783352bb72a1a68f3996b3b05d6efd6da
tLangelo <theislangelo@gmail.com>
2022-10-05 10:32:04 +0200

    few funcs in showservice

--- src/main/java/com/kinoxp/services/ShowService.java
+++ src/main/java/com/kinoxp/services/ShowService.java
@@ -1,5 +1,8 @@
 package com.kinoxp.services;
 
+import com.kinoxp.entities.Movie;
+import com.kinoxp.entities.ShowingTimes;
+import com.kinoxp.entities.Theaters;
 import com.kinoxp.repositories.ShowRepository;
 import com.kinoxp.dto.ShowResponse;
 import com.kinoxp.entities.Show;
@@ -26,4 +29,17 @@ public class ShowService {
                 .collect(Collectors.toList());
         return showResponses;
     }
+
+    public void editTheater(Theaters theater){
+
+    }
+
+    public void editShowingTime(ShowingTimes showingTime){
+
+    }
+
+    public void editMovie(Movie movie){
+
+    }
+
 }
