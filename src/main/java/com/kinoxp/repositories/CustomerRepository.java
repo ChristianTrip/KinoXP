617c62959a6bc1c0275cc17cadb7395e18585ce9
Jacob Uhlig <jacobuhlig@gmail.com>
2022-10-14 11:55:54 +0200

    some tests in related to CustomerService

--- src/main/java/com/kinoxp/repositories/CustomerRepository.java
+++ src/main/java/com/kinoxp/repositories/CustomerRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, String> {
-
-    Optional<Customer> findByEmail(String email);
 }
